7f06923f9af512b1fb35c5ddfe8627bbe3b44477
PierPankratc <peterpankratc@gmail.com>
2026-07-09 15:31:25 +0300

    2

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ def logger(old_function: callable):
     @wraps(old_function)
     def new_function(*args, **kwargs):
         start = datetime.now()
-        sleep(3)
         try:
             result = old_function(*args, **kwargs)
         except Exception as e:
@@ -59,3 +58,70 @@ def test_1():
 
 if __name__ == '__main__':
     test_1()
+
+
+
+def logger(path):
+    def __logger(old_function):
+        @wraps(old_function)
+        def new_function(*args, **kwargs):
+            start = datetime.now()
+            try:
+                result = old_function(*args, **kwargs)
+            except Exception as e:
+                result = e.__class__.__name__
+            args_ = ', '.join(repr(arg) for arg in args) if args else []
+            kwargs_ = ', '.join(f'{k} = {repr(v)}' for k, v in kwargs.items()) if kwargs else {}
+            end = datetime.now()
+            time = end - start
+            info = f'дата: {datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}, {old_function.__name__}, args = {args_}, kwargs = {kwargs_}, result = {result}, время выполнения: {time}\n'
+            with open(path,  'a', encoding='utf-8') as f:
+                f.write(info)
+            return result
+        return new_function
+    return __logger
+
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_2()
+
